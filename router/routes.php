6e2e7dd66f31f559eccd49783cf27faee6daa99d
hdial <hdiallo224@gmail.com>
2023-03-22 16:16:56 -0400

    License Tracking CRUD update

--- router/routes.php
+++ router/routes.php
@@ -11,6 +11,7 @@
     get('/edit-lic', './controllers/edit_license.php');
     get('/add-lic', './controllers/add_license.php');
     get('/delete-lic', './controllers/delete_license.php');
+    get('/view-lic', './controllers/view_license.php');
 
     if(isset($_SESSION['user_roles']) && checkPrivilege('view_users', $_SESSION['user_roles'])){
         get('/users', './controllers/users.php');
@@ -43,6 +44,7 @@
     post('/edit-lic', './controllers/edit_license.php');
     post('/add-lic', './controllers/add_license.php');
     post('/delete-lic', './controllers/delete_license.php');
+    post('/view-lic', './controllers/view_license.php');
 
     // For GET or POST
     // The 404.php which is inside the views folder will be called
